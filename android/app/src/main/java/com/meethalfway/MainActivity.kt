84bb922b5228c675bbe00c241b408b75349dfbc2
Roshan Thomas <roshanthomas@Roshans-MacBook.local>
2025-04-02 17:20:56 -0700

    fixing build properties

--- android/app/src/main/java/com/meethalfway/MainActivity.kt
+++ android/app/src/main/java/com/meethalfway/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yourcompany.meethalfway
+package com.meethalfway
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/meethalfway/MainApplication.kt
+++ android/app/src/main/java/com/meethalfway/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yourcompany.meethalfway
+package com.meethalfway
 
 import android.app.Application
 import android.content.res.Configuration
